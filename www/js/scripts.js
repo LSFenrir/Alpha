4f67018ea952aba9e66c3df02e4d045981ea2097
Robin Holzwarth <rholzwar@stud.hs-heilbronn.de>
2016-06-18 15:36:04 +0200

    Cleanup

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -3,18 +3,6 @@ var v_location = ["Standort 1", "Standort 2", "Standort 3", "Standort 4", "Stand
 var v_street = ["Straße 1", "Straße 2", "Straße 3", "Straße 4", "Straße 5"];
 var v_postcode = ["PLZ 1 Ort 1", "PLZ 2 Ort 2", "PLZ 3 Ort 3", "PLZ 4 Ort 4", "PLZ 5 Ort 5"];
 
-function init() {
-
-    //*$.event.special.swipe.scrollSupressionThreshold = 10; // More than this horizontal displacement, and we will suppress scrolling.
-    //$.event.special.swipe.horizontalDistanceThreshold = 30; // Swipe horizontal displacement must be more than this.
-    //$.event.special.swipe.durationThreshold = 500;  // More time than this, and it isn't a swipe.
-    //$.event.special.swipe.verticalDistanceThreshold = 75; // Swipe vertical displacement must be less than this.
-
-    //$("body").on("swiperight", swiperighthandler);
-    //$("body").on("swipeleft", swipelefthandler);
-
-
-}
 
 var map;
 
