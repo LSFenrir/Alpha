a28a5f3203685ae7485b1a0c0e6c4ad7348fa825
Robin Holzwarth <rholzwar@stud.hs-heilbronn.de>
2016-06-21 23:04:30 +0200

    Android app neu erstellt

--- platforms/android/assets/www/js/map.js
+++ platforms/android/assets/www/js/map.js
@@ -6,7 +6,7 @@ var map, marker, current_lat, current_lon;
 var lat_Gebaeude = [49.122537, 49.148578, 49.275522, 49.112540, 49.154460];
 var len_Gebaude = [9.210931, 9.216501, 9.712354, 9.743762, 9.208087];
 var sname =  ["Campus Heilbronn - Sontheim", "Campus Heilbronn - Am Europaplatz", "Campus Künzelsau - Reinhold-Würth-Hochschule", "Campus Schwäbisch Hall", "Forschungsinkubator"];
-var sstraße = ["Max-Planck-Straße 39", "Am Europaplatz 11", "Daimlerstraße 35", "Ziegeleiweg 4", "Im Zukunftspark 10"];
+var sstrasse = ["Max-Planck-Straße 39", "Am Europaplatz 11", "Daimlerstraße 35", "Ziegeleiweg 4", "Im Zukunftspark 10"];
 var sort = ["74081 Heilbronn", "74076 Heilbronn", "74653 Künzelsau", "74523 Schwäbisch Hall", "74076 Heilbronn"];
 
 
@@ -15,28 +15,52 @@ var directionsDisplay ;
 
 function initMap() {
 
-    directionsService = new google.maps.DirectionsService;
-    directionsDisplay = new google.maps.DirectionsRenderer;
 
-navigator.geolocation.getCurrentPosition(function (position) {
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
 
-    current_lat = position.coords.latitude;
-    current_lon = position.coords.longitude;
+    navigator.geolocation.getCurrentPosition(function (position) {
+
+        current_lat = position.coords.latitude;
+        current_lon = position.coords.longitude;
 
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: current_lat, lng: current_lon},
-        zoom: 18
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: current_lat, lng: current_lon},
+            zoom: 18
+        });
+
+
+        marker = new google.maps.Marker({
+            position: {lat: current_lat, lng: current_lon},
+            map: map,
+            title: 'Position'
+        });
+
+
+        google.maps.event.addListenerOnce(map, 'tilesloaded', checkroute());
     });
 
-    marker = new google.maps.Marker({
-        position: {lat: current_lat, lng: current_lon},
-        map:map,
-        title:'Position'
-    })
-
-})
+    //
 }
+function checkroute (){
+    /* Check for route*/
+    if (localStorage.route>0)
+    {
+        calcroute(localStorage.route);
+        localStorage.route=0;
+
+    }
+
+}
+
+function calc(route_number)
+{
+    localStorage.route=route_number;
+    window.location.href="Routenberechnung.html";
+}
+
+
 
 
 function close_prev(){
@@ -63,7 +87,7 @@ function standgebaeude(standort) {
     });
 
     $("dt.name").html(sname[standort-1]);
-    $("dd.straße").html(sstraße[standort-1]);
+    $("dd.straße").html(sstrasse[standort-1]);
     $("dd.ort").html(sort[standort-1]);
     $(".preview_container").css("display","block");
 
@@ -73,21 +97,23 @@ function standgebaeude(standort) {
 
 function calcroute(standort){
 
-    directionsDisplay.setMap(map);
+
     calculateAndDisplayRoute(lat_Gebaeude[standort],len_Gebaude[standort]);
+    directionsDisplay.setMap(map);
 
 }
 
 function calculateAndDisplayRoute(lat,len) {
-    directionsService.route({
-        origin: {lat:current_lat,lng:current_lon},
+
+    var request = {
+        origin:{lat:current_lat,lng:current_lon},
         destination: {lat:lat,lng:len},
         travelMode: google.maps.TravelMode.DRIVING
-    }, function(response, status) {
+    };
+
+    directionsService.route(request, function(result, status) {
         if (status === google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-        } else {
-            window.alert('Directions request failed due to ' + status);
+            directionsDisplay.setDirections(result);
         }
     });
 }
